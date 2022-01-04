b12058e2056a16071ea7dafff18299793c866fc0
Janne Mattila <janne.mattila@hotmail.fi>
2022-01-04 13:19:06 +0200

    Add example spoke scanning script

--- deploy/firewall/rulecollectiongroups/5-spoke/scan-spokes.ps1
+++ deploy/firewall/rulecollectiongroups/5-spoke/scan-spokes.ps1
@@ -1,7 +1,13 @@
 # https://docs.microsoft.com/en-us/azure/governance/resource-graph/samples/starter
 Install-Module -Name Az.ResourceGraph
 
-$query = @"
+# Disable Ansi output
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_ansi_terminals
+$PSStyle.OutputRendering = "PlainText"
+
+Set-Location .\deploy\firewall\rulecollectiongroups\5-spoke
+
+$queryTagVNETs = @"
 Resources 
 | where type =~ 'Microsoft.Network/virtualNetworks' 
 | where isnotnull(tags['azfw-mapping']) 
@@ -10,22 +16,55 @@ Resources
 | project-rename  Subscription = subscriptionId, ResourceGroup = resourceGroup, Name = name, Mapping = mapping, Location = location
 | order by Subscription, ResourceGroup, Name
 "@
-$query
+$queryTagVNETs
 
-$spokes = Search-AzGraph -Query $query
+$spokes = Search-AzGraph -Query $queryTagVNETs
 $spokes | Format-Table
 $spokes | Format-Table -GroupBy "Location"
 
 "# Spokes" > spokes.md
+"" >> .\spokes.md
 "Generated $(Get-date)" >> spokes.md
-
-"## Spokes by Subscription" >> spokes.md
-"``````powershell" >> spokes.md
-$spokes | Format-Table -GroupBy "Subscription" >> spokes.md
+"" >> .\spokes.md
+"## ``azfw-mapping`` tagged spokes by Subscription" >> spokes.md
+"" >> .\spokes.md
+"``````text" >> spokes.md
+$spokes | Format-Table -GroupBy "Subscription" | Out-File -FilePath spokes.md -Append
 "``````" >> spokes.md
-
-"## Spokes by Location" >> spokes.md
-"``````powershell" >> spokes.md
+"" >> .\spokes.md
+"## ``azfw-mapping`` tagged spokes by Location" >> spokes.md
+"``````text" >> spokes.md
 $spokes | Format-Table -GroupBy "Location" >> spokes.md
 "``````" >> spokes.md
+"" >> .\spokes.md
 
+$hubVnet = Get-AzVirtualNetwork -Name "vnet-hub" -ResourceGroupName "rg-azure-firewall-demo"
+$hubVnet.Id
+$queryPeeredVNETs = @"
+Resources
+| where type =~ "microsoft.network/virtualNetworks"
+| mv-expand peering=properties.virtualNetworkPeerings
+| where peering.properties.remoteVirtualNetwork.id == "$($hubVnet.Id)"
+| extend mapping = tags['azfw-mapping']
+| project  subscriptionId, resourceGroup, name, mapping, location
+| project-rename  Subscription = subscriptionId, ResourceGroup = resourceGroup, Name = name, Mapping = mapping, Location = location
+| order by Subscription, ResourceGroup, Name
+"@
+$queryPeeredVNETs
+
+$allSpokes = Search-AzGraph -Query $queryPeeredVNETs
+$allSpokes | Format-Table
+$allSpokes | Format-Table -GroupBy "Location"
+
+"" >> .\spokes.md
+"## All spokes by Subscription" >> spokes.md
+"" >> .\spokes.md
+"``````text" >> spokes.md
+$allSpokes | Format-Table -GroupBy "Subscription" | Out-File -FilePath spokes.md -Append
+"``````" >> spokes.md
+"" >> .\spokes.md
+"## All spokes by Location" >> spokes.md
+"``````text" >> spokes.md
+$allSpokes | Format-Table -GroupBy "Location" >> spokes.md
+"``````" >> spokes.md
+"" >> .\spokes.md
---
+++ deploy/firewall/rulecollectiongroups/5-spoke/spokes.md
@@ -0,0 +1,59 @@
+# Spokes
+
+Generated 01/04/2022 13:17:32
+
+## `azfw-mapping` tagged spokes by Subscription
+
+```text
+
+   Subscription: 67b24150-18e9-4c69-995f-e68c06ad5fc8
+
+Subscription                         ResourceGroup          Name          Mapping  Location
+------------                         -------------          ----          -------  --------
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke003 spoke003 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke002 spoke002 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke001 spoke001 northeurope
+
+```
+
+## `azfw-mapping` tagged spokes by Location
+```text
+
+   Location: northeurope
+
+Subscription                         ResourceGroup          Name          Mapping  Location
+------------                         -------------          ----          -------  --------
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke003 spoke003 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke002 spoke002 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke001 spoke001 northeurope
+
+```
+
+
+## All spokes by Subscription
+
+```text
+
+   Subscription: 67b24150-18e9-4c69-995f-e68c06ad5fc8
+
+Subscription                         ResourceGroup          Name          Mapping  Location
+------------                         -------------          ----          -------  --------
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke003 spoke003 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke002 spoke002 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke001 spoke001 northeurope
+
+```
+
+## All spokes by Location
+```text
+
+   Location: northeurope
+
+Subscription                         ResourceGroup          Name          Mapping  Location
+------------                         -------------          ----          -------  --------
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke003 spoke003 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke002 spoke002 northeurope
+67b24150-18e9-4c69-995f-e68c06ad5fc8 rg-azure-firewall-demo vnet-spoke001 spoke001 northeurope
+
+```
+
