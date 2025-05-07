43d37a459c8fb96b27979eeb8b64550456966dad
Janne Mattila <janne.mattila@hotmail.fi>
2025-05-07 09:12:11 +0300

    Update Windows deployment script

--- deploy/infrastructure/spoke/install-windows.ps1
+++ deploy/infrastructure/spoke/install-windows.ps1
@@ -16,15 +16,19 @@ Set-Location \temp\
 # The .NET Core Hosting Bundle
 # https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/hosting-bundle?view=aspnetcore-9.0
 # https://dotnet.microsoft.com/permalink/dotnetcore-current-windows-runtime-bundle-installer
-Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.2/dotnet-hosting-9.0.2-win.exe" -OutFile dotnet-hosting.exe
-.\dotnet-hosting.exe /quiet
+$DOTNET_HOSTING_VERSION = "9.0.4"
+Write-Host "Installing .NET Hosting Bundle version: $DOTNET_HOSTING_VERSION"
+Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$DOTNET_HOSTING_VERSION/dotnet-hosting-$DOTNET_HOSTING_VERSION-win.exe" -OutFile dotnet-hosting.exe
+Start-Process dotnet-hosting.exe -Wait -ArgumentList /quiet
+Remove-Item dotnet-hosting.exe -Force
 
+Write-Host "Installing WebApp Network Tester"
 Invoke-WebRequest "https://github.com/JanneMattila/webapp-network-tester/releases/latest/download/webappnetworktester.zip" -OutFile webapp-network-tester.zip
 Expand-Archive webapp-network-tester.zip
-
-# Copy the webapp-network-tester to the IIS
-Remove-Item C:\inetpub\wwwroot\* -Recurse -Force
-Copy-Item -Path webapp-network-tester\artifacts\webappnetworktester\* -Destination C:\inetpub\wwwroot\ -Recurse -Force
+Remove-Item webapp-network-tester.zip -Force
+Remove-Item C:/inetpub/wwwroot/* -Recurse -Force
+Copy-Item -Path webapp-network-tester/artifacts/webappnetworktester/* -Destination C:/inetpub/wwwroot/ -Recurse -Force
+Remove-Item webapp-network-tester -Recurse -Force
 
 # Force the IIS to restart
 net stop was /y
